96e8589174f070c34393c6a4bf1948cda47e33bc
yongshuang <yongshuang@gdgyun.com>
2019-04-12 18:27:36 +0800

    commit

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,15 +4,15 @@ wx.cloud.init({
 const db = wx.cloud.database({
   env:'cd-travel-de6a49'
 })
-console.log(db)
+// console.log(db)
 
-const article = db.collection('article')
-console.log(article)
+// const article = db.collection('article')
+// console.log(article)
 
-db.collection('article').get().then(res => {
-  // res.data 包含该记录的数据
-  console.log(res)
-})
+// db.collection('article').get().then(res => {
+//   // res.data 包含该记录的数据
+//   console.log(res)
+// })
 
 
 
